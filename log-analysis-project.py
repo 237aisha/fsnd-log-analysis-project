2b35efc66f215d7fffb8b0cea3411d3c69c17ac2
AishaAlghamdi <33966252+237aisha@users.noreply.github.com>
2018-12-29 15:38:22 +0300

    Update log-analysis-project.py

--- log-analysis-project.py
+++ log-analysis-project.py
@@ -1,7 +1,6 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python3
 import psycopg2
 
-    
 
 def main():
     q1()
@@ -14,15 +13,15 @@ def q1():
     db = psycopg2.connect("dbname=news")
     c = db.cursor()
     # first query
-    Q1 = """SELECT title, count(*) AS views 
-    FROM articles, log 
+    Q1 = """SELECT title, count(*) AS views
+    FROM articles, log
     WHERE concat('/article/', articles.slug) = log.path
-    GROUP BY log.path, articles.title 
+    GROUP BY log.path, articles.title
     ORDER BY views DESC LIMIT 3;"""
     # excute and fetch querie
     c.execute(Q1)
     result = c.fetchall()
-    # print the result 
+    # print the result
     print("Most popular three articles of all time: ")
     for i in result:
         print("- {} -- {} {}".format(str(i[0]), str(i[1]), 'views'))
@@ -30,16 +29,16 @@ def q1():
     # close connection
     c.close()
     db.close()
-    
+
 
 def q2():
     # connect to the database
     db = psycopg2.connect("dbname=news")
     c = db.cursor()
     # second query
-    Q2 = """SELECT authors.name, count(*) AS views 
-    FROM authors,articles,log 
-    WHERE articles.author = authors.id 
+    Q2 = """SELECT authors.name, count(*) AS views
+    FROM authors,articles,log
+    WHERE articles.author = authors.id
     AND concat('/article/', articles.slug) = log.path
     GROUP BY authors.name
     ORDER BY views DESC LIMIT 3;"""
@@ -61,12 +60,12 @@ def q3():
     db = psycopg2.connect("dbname=news")
     c = db.cursor()
     # third query
-    Q3 = """SELECT count_date.date , 
-            (100.00*count_error.errors/count_date.count) AS percent_error 
-            FROM count_error,count_date
-            WHERE count_date.date = count_error.date 
-            AND (100.00*count_error.errors/count_date.count) >1
-            ORDER BY date;"""
+    Q3 = """SELECT count_date.date ,
+    (100.00*count_error.errors/count_date.count) AS percent_error
+    FROM count_error,count_date
+    WHERE count_date.date = count_error.date
+    AND (100.00*count_error.errors/count_date.count) > 1
+    ORDER BY date;"""
     # excute and fetch querie
     c.execute(Q3)
     result = c.fetchall()
@@ -77,7 +76,6 @@ def q3():
     c.close()
     db.close()
 
+
 if __name__ == "__main__":
     main()
-
-
